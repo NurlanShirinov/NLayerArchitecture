12bafe2b8349f658414a6e1fa033e820c7566f4e
NurlanShirinov <nurlan.shirinov1998@gmail.com>
2023-05-14 13:50:38 +0400

    still not done

---
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using NLayer.API.Filters;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+
+    public class CategoriesController : CustomBaseController
+    {
+
+        private readonly ICategoryService _scategoryService;
+
+        public CategoriesController(ICategoryService scategoryService)
+        {
+            _scategoryService = scategoryService;
+        }
+
+
+        // api/categories/GetSingleCategoryByIdWithProducts/2
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetSingleCategoryByIdWithProducts(int categoryId)
+        {
+            return CreateActionResult(await _scategoryService.GetSingleCategoryByIdWithProductsAsync(categoryId));
+        }
+
+    }
+}
---
+++ NLayer.API/Modules/RepoServiceModule.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using NLayer.Repository;
+using NLayer.Service.Mapping;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace NLayer.API.Modules
+{
+    public class RepoServiceModule:Module
+    {
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            var apiAssembly = Assembly.GetExecutingAssembly();
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly,serviceAssembly).Where(x=>x.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerLifetimeScope().InstancePerMatchingLifetimeScope();
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")) 
+                .AsImplementedInterfaces().InstancePerLifetimeScope().InstancePerMatchingLifetimeScope();
+
+
+            //InstancePerLifetimeScope => ASP.NET -de Scope uygundur
+            //InstancePerDependency => transient - uygun gelir
+
+
+
+
+
+        }
+    }
+}
