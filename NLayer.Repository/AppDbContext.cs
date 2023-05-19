d3c0d438fdea35fd2e9a6fb7004c5cae7a2befe3
Nurlan <nurlan.shirinov1998@gmiail.com>
2023-05-19 18:29:32 +0400

    AspNetMVC finished

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -15,13 +15,63 @@ namespace NLayer.Repository
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
 
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreatedDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+
+                                Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+                                entityReference.UpdatedDate = DateTime.Now;
+                                break;
+                            }
+                    }
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if(item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.Entity)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreatedDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                entityReference.UpdatedDate = DateTime.Now;
+                                break;
+                            }
+                    }
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); // Assembly-e muraciet et hansi ki execute etdiyim assembly-lere. onlarda Configuration folderinde yazdiqlarimdir. her birin ishledecek
             //modelBuilder.ApplyConfiguration(new ProductConfiguration());    //Yalniz bir configuration ishletmek isdedikde bu formada yazilir. lakin configurationlarim coxlu olduguna gore yuxaridaki kod daha best practice dir.
-
-
-
 
             //ProductFeatyre üçün yazılan bu kodu Seeds folderinin daxilində digər CatergorySeed və ProductSeed
             //kimi də ayrı klassda yaza bilərdik bu formada yazildigini gotmek ucun burda yazdiq.
